48160d124330aff81704247fcc6f7908913c5c0a
Екатерина <159999371+Catrinfokina@users.noreply.github.com>
2024-04-03 14:16:51 +0300

    Delete uber eats directory

--- uber eats/js/main.js
+++
@@ -1,9 +0,0 @@
-window.onscroll = function showHeader (){
-	var header = document.querySelector ('.header');
-	if(window.pageYOffset > 50){
-		header.classList.add('header_active');
-	}
-	else{
-		header.classList.remove('header_active');
-	}
-}
